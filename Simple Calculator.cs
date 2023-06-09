98184502d0b28262e18d98aaa891774b83fc0780
MadKingAlex <129024859+MadKingAlex@users.noreply.github.com>
2023-06-08 23:14:22 -0400

    Add files via upload

---
+++ Simple Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The user puts their two in here
+            Console.WriteLine("Enter your first number:");
+           string inputNumber1 = Console.ReadLine();
+            Console.WriteLine("Enter your second number:");
+           string inputNumber2 = Console.ReadLine();
+           
+            //We need to convert string to double because yes and we want the precision because decimal numbers
+            double number1 = Convert.ToInt64(inputNumber1);
+            double number2 = Convert.ToInt64(inputNumber2);
+
+            //Now is time for da math
+            Console.WriteLine("Sum: " + (number1 + number2));
+            Console.WriteLine("Difference: " + (number1 - number2));
+            Console.WriteLine("Product: " + (number1 * number2));
+            Console.WriteLine("Quotient: " + (number1 / number2));
+            
+                Console.ReadKey();
+        }
+    }
+}
